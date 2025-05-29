62e2307b77e43cad1b2b388e2d7a401451d93cf0
aspirantzhang <admin@aspirantzhang.com>
2025-05-29 20:28:56 +0800

    feat: add create action to header in ListPages

--- src/Resources/PageResource/Pages/ListPages.php
+++ src/Resources/PageResource/Pages/ListPages.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentInfoPages\Resources\PageResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use RectitudeOpen\FilamentInfoPages\Resources\PageResource;
 
 class ListPages extends ListRecords
 {
     protected static string $resource = PageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
