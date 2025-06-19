aec2b3fbd6fe00da79ab195f7b9cc4f835191c49
aspirantzhang <admin@aspirantzhang.com>
2025-06-19 21:40:59 +0800

    feat: implement header actions for EditPage with revisions and delete options

--- src/Resources/PageResource/Pages/EditPage.php
+++ src/Resources/PageResource/Pages/EditPage.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentInfoPages\Resources\PageResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Mansoor\FilamentVersionable\Page\RevisionsAction;
 use RectitudeOpen\FilamentInfoPages\Resources\PageResource;
 
 class EditPage extends EditRecord
 {
     protected static string $resource = PageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            RevisionsAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
