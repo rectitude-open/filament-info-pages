5aaf181d0dc9225ec903001c7c9fe0c0322c1ebf
aspirantzhang <admin@aspirantzhang.com>
2025-07-09 12:31:52 +0800

    feat: add getAllLatestPublishedPages method and unify model retrieval method name to getModel

--- src/FilamentInfoPages.php
+++ src/FilamentInfoPages.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentInfoPages;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use RectitudeOpen\FilamentInfoPages\Models\Page;
 
@@ -12,7 +13,7 @@ class FilamentInfoPages
     /**
      * @return class-string<Page>
      */
-    public function getPageModel(): string
+    public function getModel(): string
     {
         return config('filament-info-pages.model', Page::class);
     }
@@ -22,7 +23,7 @@ class FilamentInfoPages
      */
     public function getPublishedPageByIdOrFail(int $id): Page
     {
-        $model = $this->getPageModel();
+        $model = $this->getModel();
 
         $page = $model::published()->findOrFail($id);
 
@@ -36,12 +37,23 @@ class FilamentInfoPages
      */
     public function getPublishedPageBySlugOrFail(string $slug): Page
     {
-        $model = $this->getPageModel();
+        $model = $this->getModel();
 
         $page = $model::published()->withSlug($slug)->firstOrFail();
 
         assert($page instanceof Page);
 
         return $page;
+    }
+
+    /**
+     * @return Collection<int, Page>
+     */
+    public function getAllLatestPublishedPages(): Collection
+    {
+        $pages = $this->getModel()::published()->latest();
+
+        /** @var Collection<int, Page> $pages */
+        return $pages;
     }
 }
